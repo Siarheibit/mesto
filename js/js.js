fab6309767213e573fc3adea5bad6c151cbacef7
Siarhei Baranau <molderpp@gmail.com>
2021-02-23 14:12:20 +0300

    two

--- js/js.js
+++ js/js.js
@@ -1,35 +1,36 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+
 function openPopup() {
-  popup.classList.add('popup_opened');
-  console.log(popup.className);
+    popup.classList.add('popup_opened');
+    console.log(popup.className);
 }
-editButton.addEventListener('click', openPopup); 
+editButton.addEventListener('click', openPopup);
 
-                                              //
+//
 
-let newName = document.querySelector('.popup__text_name'); 
+let newName = document.querySelector('.popup__text_name');
 let newJob = document.querySelector('.popup__text_job');
 let buttonSave = document.querySelector('.popup__button-save');
 let title = document.querySelector('.profile__title');
 let subTitle = document.querySelector('.profile__subtitle');
 
 function rename() {
-  title.textContent = (newName.value);
-  subTitle.textContent = (newJob.value);
-  popup.classList.remove('popup_opened');
+    title.textContent = (newName.value);
+    subTitle.textContent = (newJob.value);
+    popup.classList.remove('popup_opened');
 }
 buttonSave.addEventListener('click', rename);
 
 
 
-let heart = document.addEventListener("click", function (id) {
-  let heartId = id.target.getAttribute('id');
+let heart = document.addEventListener("click", function(id) {
+    let heartId = id.target.getAttribute('id');
 
-  if(heartId > 0 && heartId < 7){
-  let object = document.getElementById(heartId);
-  object.setAttribute('src', './images/heart-black.png');
-  }
+    if (heartId > 0 && heartId < 7) {
+        let object = document.getElementById(heartId);
+        object.setAttribute('src', './images/heart-black.svg');
+    }
 
 
 });
@@ -37,7 +38,8 @@ let heart = document.addEventListener("click", function (id) {
 
 
 let buttonClose = document.querySelector('.popup__button_close');
+
 function close() {
-  popup.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 buttonClose.addEventListener('click', close);
