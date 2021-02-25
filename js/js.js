53775f99baffe01b1774e501b862de71ff2a7ca9
Siarhei Baranau <molderpp@gmail.com>
2021-02-25 23:11:15 +0300

    переделка popup

--- js/js.js
+++ js/js.js
@@ -1,45 +1,30 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-
-function openPopup() {
-    popup.classList.add('popup_opened');
-    console.log(popup.className);
-}
-editButton.addEventListener('click', openPopup);
-
-//
-
 let newName = document.querySelector('.popup__text_name');
 let newJob = document.querySelector('.popup__text_job');
 let buttonSave = document.querySelector('.popup__button-save');
+
 let title = document.querySelector('.profile__title');
 let subTitle = document.querySelector('.profile__subtitle');
+let buttonClose = document.querySelector('.popup__button-close');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+}
+
+function close() {                              
+    popup.classList.remove('popup_opened');
+}
+
 
 function rename() {
     title.textContent = (newName.value);
     subTitle.textContent = (newJob.value);
-    popup.classList.remove('popup_opened');
-}
+   close();
+   event.preventDefault();
+   }
+
+
+editButton.addEventListener('click', openPopup);
 buttonSave.addEventListener('click', rename);
-
-
-
-let heart = document.addEventListener("click", function(id) {
-    let heartId = id.target.getAttribute('id');
-
-    if (heartId > 0 && heartId < 7) {
-        let object = document.getElementById(heartId);
-        object.setAttribute('src', './images/heart-black.svg');
-    }
-
-
-});
-
-
-
-let buttonClose = document.querySelector('.popup__button-close');
-
-function close() {
-    popup.classList.remove('popup_opened');
-}
 buttonClose.addEventListener('click', close);
