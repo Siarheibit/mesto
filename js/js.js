0e684ff71c749c2e9ada73c57a65602bec4f5705
siarhei Baranau <molderpp@gmail.com>
2021-02-26 17:12:09 +0300

    исправил: попап, кнопки, лайки, 320px

--- js/js.js
+++ js/js.js
@@ -1,30 +1,27 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let newName = document.querySelector('.popup__text_name');
-let newJob = document.querySelector('.popup__text_job');
-let buttonSave = document.querySelector('.popup__button-save');
-
+let newName = document.querySelector('.text_name');
+let newJob = document.querySelector('.text_job');
 let title = document.querySelector('.profile__title');
 let subTitle = document.querySelector('.profile__subtitle');
 let buttonClose = document.querySelector('.popup__button-close');
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    newName.value = title.textContent;
+    newJob.value = subTitle.textContent;
 }
 
-function close() {                              
+function close() {
     popup.classList.remove('popup_opened');
 }
 
+editButton.addEventListener('click', openPopup);
+buttonClose.addEventListener('click', close);
 
-function rename() {
+document.querySelector('.popup__form').addEventListener('submit', function (event) {
     title.textContent = (newName.value);
     subTitle.textContent = (newJob.value);
-   close();
-   event.preventDefault();
-   }
-
-
-editButton.addEventListener('click', openPopup);
-buttonSave.addEventListener('click', rename);
-buttonClose.addEventListener('click', close);
+    close();
+    event.preventDefault();
+});
