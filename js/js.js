f1c6098454478084d43d3463661827a3af7530ca
Siarhei Baranau <molderpp@gmail.com>
2021-02-27 15:03:03 +0300

    исправил отправку формы

--- js/js.js
+++ js/js.js
@@ -1,27 +1,33 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let newName = document.querySelector('.popup__text_name_value');
-let newJob = document.querySelector('.popup__text_job_value');
-let title = document.querySelector('.profile__title');
-let subTitle = document.querySelector('.profile__subtitle');
-let buttonClose = document.querySelector('.popup__button-close');
+let editButton = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let newName = document.querySelector(".popup__text_name_value");
+let newJob = document.querySelector(".popup__text_job_value");
+let title = document.querySelector(".profile__title");
+let subTitle = document.querySelector(".profile__subtitle");
+let buttonClose = document.querySelector(".popup__button-close");
+let popup__form = document.querySelector(".popup__form");
 
 function openPopup() {
-    popup.classList.add('popup_opened');
-    newName.value = title.textContent;
-    newJob.value = subTitle.textContent;
+  popup.classList.add("popup_opened");
+  newName.value = title.textContent;
+  newJob.value = subTitle.textContent;
 }
 
 function close() {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove("popup_opened");
 }
 
-editButton.addEventListener('click', openPopup);
-buttonClose.addEventListener('click', close);
+function rename() {
+  title.textContent = newName.value;
+  subTitle.textContent = newJob.value;
+  close();
+}
 
-document.querySelector('.popup__form').addEventListener('submit', function (event) {
-    title.textContent = (newName.value);
-    subTitle.textContent = (newJob.value);
-    close();
-    event.preventDefault();
+popup__form.addEventListener("submit", function (event) {
+  rename();
+  event.preventDefault();
 });
+
+
+buttonClose.addEventListener("click", close);
+editButton.addEventListener("click", openPopup);
